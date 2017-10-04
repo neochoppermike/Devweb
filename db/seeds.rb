d7b4440a49800fce9f8c32483a8c2625472f8319
Michael Amos <zuzuspetals@hotmail.co.uk>
2017-10-04 10:11:24 -0400

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(name: "Daisy", description: "An entry-level cow, perfect for butter and annoying the neighbours.", image_url: cow_3.jpg, price: 999)
-Product.create(name: "Daisy", description: "A Highland cow that won't take up the bathroom in the morning. She doesn't care how she looks and doesn't mind telling you.", image_url: cow_7.jpg, price: 1250)
-Product.create(name: "Daisy", description: "Bob is an intense cow. He can frequently be found talking to himself in the mirror in an angry fashion: \"You milkin me? Are YOU milkin ME? Well I don't see anyone ELSE standin here...\" Despite his anger, he's a hit with the younger generation because he can tie balloon animals.", image_url: cow_4.jpg, price: 1500)
-Product.create(name: "Daisy", description: "Dave came to us when he was three years old from a broken farm and had clearly had a troubled upbringing. He can be rambunctious, but has a heart of gold and almost never attacks people. Any more. Would make an ideal guard-cow. Dave's the one on the left.", image_url: cow_5.jpg, price: 2500)
-Product.create(name: "Daisy", description: "Make no mistakes, Marigold is a killer. A stone-cold killer. She's single-hornedly reduced the staffing here at the farm by almost half. We just want to get rid of her. To a genuine loving home.", image_url: cow_8.jpg, price: 2.99)
-Product.create(name: "Daisy", description: "Sanjit has transcended his ego and become one with his Udder-self. Requires virtually no upkeep and frequently floats back to his shed by himself with his eyes closed. ", image_url: cow_1.jpg, price: 5000)
