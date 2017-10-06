ea9a47630c9be7fcf9d6da50b7afd21cd996977c
Michael Amos <zuzuspetals@hotmail.co.uk>
2017-10-06 11:18:16 -0400

    4.6 alter datatype of price field in products table to integer

---
+++ db/migrate/20171006151003_remove_decimal_from_products.rb
@@ -0,0 +1,5 @@
+class RemoveDecimalFromProducts < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :products, :price, :decimal
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002141015) do
+ActiveRecord::Schema.define(version: 20171006151250) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20171002141015) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.decimal "price"
+    t.integer "price"
   end
 
 end
