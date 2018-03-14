63b5cf7dd65554da85fb5fd95cd67b8e18231f7f
Michael Amos <zuzuspetals@hotmail.co.uk>
2018-03-14 09:43:57 -0400

    form updated

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,11 @@
 class Product < ApplicationRecord
   def self.search(search_term)
-    Product.where("name LIKE ?", "%#{search_term}%")
+    def self.search(search_term)
+      if Rails.env.production?
+        Product.where("name ilike ?", "%#{search_term}%")
+      else
+        Product.where("name LIKE ?", "%#{search_term}%")
+    end
   end
 end
 
