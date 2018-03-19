8b32f6a886d602252c618ce1523a9829ca724206
Michael Amos <zuzuspetals@hotmail.co.uk>
2018-03-19 14:25:07 -0400

    5.3 add mailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,6 +10,18 @@ class StaticPagesController < ApplicationController
   end
 
 
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+      to: 'michael.amos@yandex.com',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+  end
+  
+
+
 end
 
 
