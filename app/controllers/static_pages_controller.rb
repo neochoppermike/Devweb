05f0c02b8a857c98d13aeb9637158be07ff10fe6
Michael Amos <zuzuspetals@hotmail.co.uk>
2018-03-20 09:10:16 -0400

    5.3 add contact form email

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,10 +14,7 @@ class StaticPagesController < ApplicationController
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
-    ActionMailer::Base.mail(from: @email,
-      to: 'michael.amos@yandex.com',
-      subject: "A new contact form message from #{@name}",
-      body: @message).deliver_now
+    UserMailer.contact_form(@email, @name, @message).deliver_now
   end
   
 
