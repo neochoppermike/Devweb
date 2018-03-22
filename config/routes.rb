bdeca8ff9f95addddb8f876416645b79779cb00a
Michael Amos <zuzuspetals@hotmail.co.uk>
2018-03-22 11:32:00 -0400

    5.4 debug heroku

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,6 @@ Rails.application.routes.draw do
 
   root 'static_pages#index'
 
-  get '/signup'
-
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
