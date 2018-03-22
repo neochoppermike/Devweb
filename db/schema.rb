056fa30c5e4a3e38f7bf33c22679d69339aa5645
Michael Amos <zuzuspetals@hotmail.co.uk>
2018-03-22 11:57:35 -0400

    5.4

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20180321143912) do
     t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    WITH (OIDS=TRUE)
   end
 
 end
