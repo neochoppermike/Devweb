ca3c241f068a83139f8e293b38d0f4744877dcf0
Michael Amos <zuzuspetals@hotmail.co.uk>
2018-03-23 09:16:44 -0400

    5.4 alter schema fix

--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 20180321143912) do
     t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    WITH (OIDS=TRUE)
   end
 
 end
