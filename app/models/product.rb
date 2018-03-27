d417cd5f202cc4be216d7eb2c28cc4ed2c6ff929
Michael Amos <zuzuspetals@hotmail.co.uk>
2018-03-27 12:29:22 -0400

    5.7 create comment model and style products index and show

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   has_many :orders  
+  has_many :comments
+  
   def self.search(search_term)
     if Rails.env.production?
       Product.where("name ilike ?", "%#{search_term}%")
@@ -7,6 +9,19 @@ class Product < ApplicationRecord
       Product.where("name LIKE ?", "%#{search_term}%")
     end  
   end
+
+  def highest_rating_comment
+  	comments.rating_desc.first
+  end	
+
+  def lowest_rating_comment
+    comments.rating_desc.last
+  end
+
+  def average_user_rating
+    comments.average(:rating).to_f
+  end  
+
 end
 
 
