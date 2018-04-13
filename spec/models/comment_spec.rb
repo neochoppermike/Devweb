ba68af3a794958e277b2feda361594deb89e30ef
Michael Amos <zuzuspetals@hotmail.co.uk>
2018-04-13 10:00:46 -0400

    6.2 add spec model files and validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 
-describe Product do
-  context "when the product has comments" do
+describe Comment do
+  context "when comment is present" do
     let(:product) { Product.create!(name: "Cowdy Partner", description: "Great big cow", image_url: "fat_cow.jpg", price: 299 ) }
     let(:user) {User.create!(first_name: "Todd", last_name: "Sampson", email: "todd@yahoo.com", password: "bozobag")}
 
@@ -12,17 +12,9 @@ describe Product do
 	  product.comments.create!(rating: 5, user: user, body: "Well this is a first rate cowwww!")
 	end
 
-	it "returns the average rating of all comments" do
-	  expect(product.average_rating).to eq 3
-	end 
-
-	it "is not valid without a name" do
-      expect(Product.new(description: "Cool cow")).not_to be_valid
+	it "is not valid without a user" do
+      expect(Comment.new(body: "Cool cow", rating: 2, product_id: 1)).not_to be_valid
 	end
-
-    it "returns the highest rating of all comments" do
-	  expect(product.highest_rating_comment.rating).to eq 5
-	end 
 
   end
 end  
