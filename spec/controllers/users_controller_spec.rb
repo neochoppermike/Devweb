f43bffa70f020712e57381eaf274cfbe34745ad4
Michael Amos <zuzuspetals@hotmail.co.uk>
2018-04-17 13:40:24 -0400

    6.4 add factorybot

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,35 +14,44 @@ describe UsersController, type: :controller do
   describe 'GET #show' do
     context 'when a user is logged in' do
 
+      before do
+      	sign_in @user
+      end  	
+
       it 'loads correct user details' do
-        get :show, params: {id: user.id}
+        get :show, params: {id: @user.id}
         expect(response).to be_ok
-        expect(assigns(:user)).to eq user
+        expect(assigns(:user)).to eq @user
       end 
     end
 
     context 'when a user is not logged in' do
       it 'redirects to root' do
-        get :show, params: {id: user.id}
+        get :show, params: {id: @user.id}
         expect(response).to redirect_to(root_path)
       end    
     end
   
     context 'when user1 is signed in' do
 
+      before do
+        sign_in @user
+      end
 
       it 'cannot access user2 data' do
-        get :show, params: {id: user2.id}
+        get :show, params: {id: @user2.id}
         expect(response).to redirect_to(root_path)
       end  
     end  
   
     context 'when user1 attempts to access user2 data' do
       
-     
+      before do
+        sign_in @user
+      end
 
       it 'redirects to root path ("redirection found" = HTTP status 302)' do
-        get :show, params: {id: user2.id}
+        get :show, params: {id: @user2.id}
         expect(response).to have_http_status(302)
       end  
     end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,11 +1,23 @@
 #no factories outside of this block!
 FactoryBot.define do
+
+  sequence(:email) { |n| "user#{n}@example.com" }
+
   factory :user do
-    email "plumbum@lead.com"
+    email 
     password "megatron"
     first_name "Peter"
     last_name "Lead"
     admin false
   end
-end
+
+  factory :admin, class: User do
+  email 
+  password "qwertyuiop"
+  admin true
+  first_name "Admin"
+  last_name "User"
+  end
+  
+end  
 #no factories outside of this block!
