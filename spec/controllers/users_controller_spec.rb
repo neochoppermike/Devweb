9a7105f646d94a0c148598929bc41e38bd2a33f9
Michael Amos <zuzuspetals@hotmail.co.uk>
2018-04-17 10:22:31 -0400

    6.3 write specsfor user authorization

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,6 +4,8 @@ require 'rails_helper'
 
 describe UsersController, type: :controller do
   let(:user) {User.create!(first_name: "Todd", last_name: "Sampson", email: "todd@yahoo.com", password: "bozobag")}
+  let(:user2) {User.create!(first_name: "Biff", last_name: "Simpson", email: "biff@yahoo.com", password: "yopeyo")}
+
 
   describe 'GET #show' do
     context 'when a user is logged in' do
@@ -20,10 +22,34 @@ describe UsersController, type: :controller do
     end
 
     context 'when a user is not logged in' do
-      it 'redirects to login' do
+      it 'redirects to root' do
         get :show, params: {id: user.id}
-        expect(response).to redirect_to(new_user_session_path)
+        expect(response).to redirect_to(root_path)
       end    
+    end
+  
+    context 'when user1 is signed in' do
+
+      before do
+        sign_in user
+      end
+
+      it 'cannot access user2 data' do
+        get :show, params: {id: user2.id}
+        expect(response).to redirect_to(root_path)
+      end  
+    end  
+  
+    context 'when user1 attempts to access user2 data' do
+      
+      before do
+        sign_in user
+      end
+
+      it 'reports UNAUTHORIZED (HTTP status 401)' do
+        get :show, params: {id: user2.id}
+        expect(response).to have_http_status(401)
+      end  
     end
   end 
 end     
