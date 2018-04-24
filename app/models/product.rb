e329b9bf688e804d74aee6ca47b3569b0bcaf078
Michael Amos <zuzuspetals@hotmail.co.uk>
2018-04-24 09:35:06 -0400

    6.11 debug search function on heroku

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,9 +10,9 @@ class Product < ApplicationRecord
   
   def self.search(search_term)
     if Rails.env.production?
-      Product.where("name ilike :search_term OR description ilike :search_term", search_term: "%#{search_term}%")
+      Product.where("name ilike ?", "%#{search_term}%")
     else
-      Product.where("name LIKE :search_term OR description LIKE :search_term", search_term: "%#{search_term}%")
+      Product.where("name LIKE ?", "%#{search_term}%")
     end  
   end
 
